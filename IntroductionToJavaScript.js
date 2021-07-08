e1fb6d31c30267b9bac6982efa770fe0ff5eb0b2
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-07-08 16:06:30 -0500

    Update IntroductionToJavaScript.js

--- IntroductionToJavaScript.js
+++ IntroductionToJavaScript.js
@@ -20,3 +20,15 @@ console.log(0.2708*100);
 
 console.log('Hello' + 'World')
 console.log('Hello' + ' ' + 'World')
+
+console.log('Teaching the world how to code'.length);
+
+// Use .toUpperCase() to log 'Codecademy' in all uppercase letters
+console.log('Codecademy'.toUpperCase());
+
+// Use a string method to log the following string without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim());
+
+console.log(Math.floor(Math.random() * 100));
+console.log(Math.ceil(43.8));
+console.log(Number.isInteger(2017));
