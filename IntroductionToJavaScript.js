24a4ca23559993ba45f1b8590959c056ee7dcbe5
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-07-08 15:50:06 -0500

    Update IntroductionToJavaScript.js

--- IntroductionToJavaScript.js
+++ IntroductionToJavaScript.js
@@ -7,3 +7,13 @@ console.log('The doctors were puzzled by the fact that it wasn\'t quite jaundice
 console.log('If it became jaundice they could treat it.');
 console.log('If it didn\'t become jaundice and went away they could discharge him.');
 console.log('But this just being short of jaundice all the time confused them.'); */
+
+console.log('JavaScript');
+console.log(2011)
+console.log('Woohoo! I love to code! #codecademy')
+console.log(20.49)
+
+console.log(40 + 3.5);
+console.log(2021-1969);
+console.log(65/240);
+console.log(0.2708*100);
