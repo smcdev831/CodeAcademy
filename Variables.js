30ddbb6b99d79b968c843ab1657c170d45e7087c
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2022-03-19 18:52:16 -0500

    Update Variables.js

--- Variables.js
+++ Variables.js
@@ -6,3 +6,29 @@ console.log(numOfSlices)
 let changeMe = true
 changeMe = false
 console.log(changeMe)
+
+const entree = 'Enchiladas'
+console.log(entree)
+entree = 'Tacos'
+
+let levelUp = 10;
+let powerLevel = 9001;
+let multiplyMe = 32;
+let quarterMe = 1152;
+
+// Use the mathematical assignments in the space below:
+
+levelUp += 5
+powerLevel -= 100
+multiplyMe *= 11
+quarterMe /= 4
+
+let gainedDollar = 3;
+let lostDollar = 50;
+
+gainedDollar ++;
+lostDollar --;
+
+let favoriteAnimal = 'slow loris'
+
+ console.log('My favorite animal: ' + favoriteAnimal)
