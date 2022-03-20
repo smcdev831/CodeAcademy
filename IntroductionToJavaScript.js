d4ffae72ccb14c28a2cb8263539d49e301e4fea3
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2022-03-19 21:16:19 -0500

    Update IntroductionToJavaScript.js

--- IntroductionToJavaScript.js
+++ IntroductionToJavaScript.js
@@ -32,3 +32,8 @@ console.log('    Remove whitespace   '.trim());
 console.log(Math.floor(Math.random() * 100));
 console.log(Math.ceil(43.8));
 console.log(Number.isInteger(2017));
+
+Quiz Summary
+100%
+Great job!
+11 correct
