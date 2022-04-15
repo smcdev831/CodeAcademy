5e9f533e7ee5f39bd3f7a2c6efcdc1cd60d6331f
smcdev <smcdev@outlook.com>
2022-04-14 23:17:51 -0500

    Update Objects.js

--- Objects.js
+++ Objects.js
@@ -3,37 +3,51 @@ let fasterShip = {
     color: 'silver'
   };
 
-  let spaceship = {
-    homePlanet: 'Earth',
-    color: 'silver',
-    'Fuel Type': 'Turbo Fuel',
-    numCrew: 5,
-    flightPath: ['Venus', 'Mars', 'Saturn']
-  };
-  
-  let crewCount = spaceship.numCrew
-  let planetArray = spaceship.flightPath
+let spaceship = {
+homePlanet: 'Earth',
+color: 'silver',
+'Fuel Type': 'Turbo Fuel',
+numCrew: 5,
+flightPath: ['Venus', 'Mars', 'Saturn']
+};
 
-  let spaceship = {
-    'Fuel Type' : 'Turbo Fuel',
-    'Active Mission' : true,
-    homePlanet : 'Earth', 
-    numCrew: 5
-   };
-  
-  let propName =  'Active Mission';
-  let isActive = spaceship.propName
+let crewCount = spaceship.numCrew
+let planetArray = spaceship.flightPath
 
-  console.log(spaceship[propName]);
+let spaceship = {
+'Fuel Type' : 'Turbo Fuel',
+'Active Mission' : true,
+homePlanet : 'Earth', 
+numCrew: 5
+};
 
-  let spaceship = {
-    'Fuel Type' : 'Turbo Fuel',
-    homePlanet : 'Earth',
-    color: 'silver',
-    'Secret Mission' : 'Discover life outside of Earth.'
-  };
-  
-  spaceship.color = 'glorious gold'
-  spaceship.numEngines = 7
+let propName =  'Active Mission';
+let isActive = spaceship.propName
 
-  delete spaceship['Secret Mission']
+console.log(spaceship[propName]);
+
+let spaceship = {
+'Fuel Type' : 'Turbo Fuel',
+homePlanet : 'Earth',
+color: 'silver',
+'Secret Mission' : 'Discover life outside of Earth.'
+};
+
+spaceship.color = 'glorious gold'
+spaceship.numEngines = 7
+
+delete spaceship['Secret Mission']
+
+let retreatMessage = 'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
+
+let alienShip = {
+    retreat() {
+        console.log(retreatMessage);
+    },
+    takeOff() {
+        console.log('Spim... Borp... Glix... Blastoff!')
+    }
+}
+
+alienShip.retreat()
+alienShip.takeOff()
