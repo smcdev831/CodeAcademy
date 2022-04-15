d41a58a97f2d737709b08e9e45b1d6294da40a87
smcdev <smcdev@outlook.com>
2022-04-14 22:33:33 -0500

    Update Iterators.js

--- Iterators.js
+++ Iterators.js
@@ -85,4 +85,43 @@ const newSum = newNumbers.reduce((accumulator, currentValue) => {
     return accumulator + currentValue
   }, 10)
   
-  console.log(newSum);
+console.log(newSum);
+
+const words = ['unique', 'uncanny', 'pique', 'oxymoron', 'guise'];
+
+console.log(words.some((word) => {
+    return word.length < 6;
+}));
+
+const interestingWords = words.filter(word => {
+    if (word.length > 5) {
+        return true
+    }
+})
+
+console.log(interestingWords.every((word) => { 
+    return word.length > 5
+} ));
+
+const cities = ['Orlando', 'Dubai', 'Edinburgh', 'Chennai', 'Accra', 'Denver', 'Eskisehir', 'Medellin', 'Yokohama'];
+
+const nums = [1, 50, 75, 200, 350, 525, 1000];
+
+//  Choose a method that will return undefined
+cities.forEach(city => console.log('Have you visited ' + city + '?'));
+
+// Choose a method that will return a new array
+const longCities = cities.filter(city => city.length > 7);
+
+// Choose a method that will return a single value
+const word = cities.reduce((acc, currVal) => {
+  return acc + currVal[0]
+}, "C");
+
+console.log(word)
+
+// Choose a method that will return a new array
+const smallerNums = nums.map(num => num - 5);
+
+// Choose a method that will return a boolean value
+nums.some(num => num < 0);
