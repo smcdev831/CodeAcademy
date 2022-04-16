a31d10e9cdebed96d746e022bfbce0cb3c49a11f
smcdev <smcdev@outlook.com>
2022-04-15 19:39:41 -0500

    Update Objects.js

--- Objects.js
+++ Objects.js
@@ -206,4 +206,68 @@ for (let crewMember in spaceship.crew) {
 
   console.log(robot.numOfSensors)
 
+  const robotFactory = (model, mobile) => {
+    return {
+      model: model,
+      mobile: mobile,
+      beep() {
+        console.log('Beep Boop');
+        }
+    }
+  }
+
+  const tinCan = robotFactory('P-500', true)
+
+  tinCan.beep()
+
+  function robotFactory(model, mobile){
+    return {
+      model,
+      mobile,
+      beep() {
+        console.log('Beep Boop');
+      }
+    }
+  }
   
+  // To check that the property value shorthand technique worked:
+  const newRobot = robotFactory('P-501', false)
+  console.log(newRobot.model)
+  console.log(newRobot.mobile)
+
+  const robot = {
+    model: '1E78V2',
+    energyLevel: 100,
+    functionality: {
+      beep() {
+        console.log('Beep Boop');
+      },
+      fireLaser() {
+        console.log('Pew Pew');
+      },
+    }
+  };
+  
+  const { functionality } = robot
+  
+  functionality.beep()
+
+  const robot = {
+    model: 'SAL-1000',
+    mobile: true,
+    sentient: false,
+    armor: 'Steel-plated',
+    energyLevel: 75
+  };
+  
+  const robotKeys = Object.keys(robot);
+  
+  console.log(robotKeys);
+
+  const robotEntries = Object.entries(robot)
+  
+  console.log(robotEntries);
+
+  const newRobot = Object.assign({laserBlaster: true, voiceRecognition: true}, robot)
+  
+  console.log(newRobot);
