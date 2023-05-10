2934d680539dc1ea6380f0b78fac23c9a55d24b9
smcdev <smcdev@outlook.com>
2023-05-10 18:25:07 -0500

    Update Conditional Statements.js

---
+++ JavaScript/Conditional Statements.js
@@ -0,0 +1,28 @@
+let sale = true;
+sale = false;
+if (sale) {
+  console.log("Time to buy!");
+}
+
+let sale = true;
+sale = false;
+if (sale) {
+  console.log("Time to buy!");
+} else {
+  console.log("Time to wait for a sale.");
+}
+
+let hungerLevel = 7;
+if (hungerLevel > 7) {
+  console.log("Time to eat!");
+} else {
+  console.log("We can eat later!");
+}
+
+let mood = "sleepy";
+let tirednessLevel = 6;
+if (mood === "sleepy" && tirednessLevel > 8) {
+  console.log("time to sleep");
+} else {
+  console.log("not bed time yet");
+}
