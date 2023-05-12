dcbba4a9b968f614480a9729f61b3be521b67e07
smcdev <smcdev@outlook.com>
2023-05-12 18:20:47 -0500

    Update Arrays.js

--- JavaScript/Arrays.js
+++ JavaScript/Arrays.js
@@ -21,3 +21,59 @@ condiments = ["Mayo"];
 console.log(condiments);
 utensils[3] = "Spoon";
 console.log(utensils);
+
+const objectives = ["Learn a new language", "Read 52 books", "Run a marathon"];
+console.log(objectives.length);
+
+const chores = ["wash dishes", "do laundry", "take out trash"];
+chores.push("feed dogs", "do pushes");
+console.log(chores);
+
+const chores = [
+  "wash dishes",
+  "do laundry",
+  "take out trash",
+  "cook dinner",
+  "mop floor",
+];
+chores.pop();
+console.log(chores);
+
+const groceryList = [
+  "orange juice",
+  "bananas",
+  "coffee beans",
+  "brown rice",
+  "pasta",
+  "coconut oil",
+  "plantains",
+];
+groceryList.shift();
+console.log(groceryList);
+groceryList.unshift("popcorn");
+console.log(groceryList);
+console.log(groceryList.slice(1, 4));
+console.log(groceryList);
+const pastaIndex = groceryList.indexOf("pasta");
+console.log(pastaIndex);
+
+const concept = ["arrays", "can", "be", "mutated"];
+function changeArr(arr) {
+  arr[3] = "MUTATED";
+}
+changeArr(concept);
+console.log(concept);
+const removeElement = (newArr) => {
+  newArr.pop();
+};
+removeElement(concept);
+console.log(concept);
+
+let numberClusters = [
+  [1, 2],
+  [3, 4],
+  [5, 6],
+];
+const target = numberClusters[2][1];
+
+Quiz: 100%
