4a0fc03f7b3134aad243abca8f728619a787de49
smcdev <smcdev@outlook.com>
2023-05-11 20:59:34 -0500

    Create Code Challenges JavaScript Fundamentals.js

---
+++ JavaScript/Code Challenges JavaScript Fundamentals.js
@@ -0,0 +1,9 @@
+// Write your function here:
+
+const greetWorld = () => {
+  return "Hello, World!";
+};
+
+console.log(greetWorld());
+
+//Write code to test your function. Even if we provide code to test your function, you should add further tests to make sure your function works correctly in all the specified situations
