6b2d20277d6b8abbfbf7de94f2810354289d6bc4
smcdev <smcdev@outlook.com>
2023-05-11 20:39:42 -0500

    Create Scope.js

---
+++ JavaScript/Scope.js
@@ -0,0 +1,52 @@
+const city = "New York City";
+function logCitySkyline() {
+  let skyscraper = "Empire State Building";
+  return "The stars over the " + skyscraper + " in " + city;
+}
+
+console.log(logCitySkyline());
+
+let satellite = "The Moon";
+let galaxy = "The Milky Way";
+let stars = "North Star";
+
+const callMyNightSky = () =>
+  "Night Sky: " + satellite + ", " + stars + ", and " + galaxy;
+
+console.log(callMyNightSky());
+
+const logVisibleLightWaves = () => {
+  const lightWaves = "Moonlight";
+  console.log(lightWaves);
+};
+
+logVisibleLightWaves();
+console.log(lightWaves);
+
+const satellite = "The Moon";
+const galaxy = "The Milky Way";
+let stars = "North Star";
+
+const callMyNightSky = () => {
+  stars = "Sirius";
+  return "Night Sky: " + satellite + ", " + stars + ", " + galaxy;
+};
+
+console.log(callMyNightSky());
+console.log(stars);
+
+const logVisibleLightWaves = () => {
+  let lightWaves = "Moonlight";
+  let region = "The Arctic";
+  // Add if statement here:
+  if (region === "The Arctic") {
+    let lightWaves = "Northern Lights";
+    console.log(lightWaves);
+  }
+  console.log(lightWaves);
+};
+
+logVisibleLightWaves();
+
+Quiz: 85%
+Quiz: 100%
