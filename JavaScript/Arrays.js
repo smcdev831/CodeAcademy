0f5997664ac554a0308e9cc88329048285020f0c
smcdev <smcdev@outlook.com>
2023-05-12 17:12:17 -0500

    Create Arrays.js

---
+++ JavaScript/Arrays.js
@@ -0,0 +1,23 @@
+const hobbies = ["coding", "singing", "talking to John"];
+console.log(hobbies);
+
+const famousSayings = [
+  "Fortune favors the brave.",
+  "A joke is a very serious thing.",
+  "Where there is love there is life.",
+];
+let listItem = famousSayings[0];
+console.log(famousSayings[2]);
+console.log(famousSayings[3]);
+
+let groceryList = ["bread", "tomatoes", "milk"];
+groceryList[1] = "avocados";
+
+let condiments = ["Ketchup", "Mustard", "Soy Sauce", "Sriracha"];
+const utensils = ["Fork", "Knife", "Chopsticks", "Spork"];
+condiments[0] = "Mayo";
+console.log(condiments);
+condiments = ["Mayo"];
+console.log(condiments);
+utensils[3] = "Spoon";
+console.log(utensils);
