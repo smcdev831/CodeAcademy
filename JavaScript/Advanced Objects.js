2e8f55c7b05218380fa3a55a9207e0846868208a
smcdev <smcdev@outlook.com>
2023-05-14 20:24:37 -0500

    Update Advanced Objects.js

--- JavaScript/Advanced Objects.js
+++ JavaScript/Advanced Objects.js
@@ -14,3 +14,13 @@ const robot = {
   },
 };
 robot.checkEnergy();
+
+const robot = {
+  _energyLevel: 100,
+  recharge() {
+    this._energyLevel += 30;
+    console.log(`Recharged! Energy is currently at ${this._energyLevel}%.`);
+  },
+};
+robot._energyLevel = "high";
+robot.recharge();
