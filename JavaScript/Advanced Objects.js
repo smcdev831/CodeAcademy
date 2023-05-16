43a5901eddabdc672ccad16d1441d0c3388865a0
smcdev <smcdev@outlook.com>
2023-05-16 00:26:40 -0500

    Update Advanced Objects.js

--- JavaScript/Advanced Objects.js
+++ JavaScript/Advanced Objects.js
@@ -71,3 +71,54 @@ const robotFactory = (model, mobile) => {
 };
 const tinCan = robotFactory("P-500", true);
 tinCan.beep();
+
+const robotFactory = (model, mobile) => {
+  return {
+    model,
+    mobile,
+    beep() {
+      console.log("Beep Boop");
+    },
+  };
+};
+// To check that the property value shorthand technique worked:
+const newRobot = robotFactory("P-501", false);
+console.log(newRobot.model);
+console.log(newRobot.mobile);
+
+const robot = {
+  model: "1E78V2",
+  energyLevel: 100,
+  functionality: {
+    beep() {
+      console.log("Beep Boop");
+    },
+    fireLaser() {
+      console.log("Pew Pew");
+    },
+  },
+};
+const { functionality } = robot;
+functionality.beep();
+
+const robot = {
+  model: "SAL-1000",
+  mobile: true,
+  sentient: false,
+  armor: "Steel-plated",
+  energyLevel: 75,
+};
+// What is missing in the following method call?
+const robotKeys = Object.keys(robot);
+console.log(robotKeys);
+// Declare robotEntries below this line:
+const robotEntries = Object.entries(robot);
+console.log(robotEntries);
+// Declare newRobot below this line:
+const newRobot = Object.assign(
+  { laserBlaster: true, voiceRecognition: true },
+  robot
+);
+console.log(newRobot);
+
+Quiz: 85%
