21165e20c0b0702a146226a665d4497d2cd05434
smcdev <smcdev@outlook.com>
2023-05-17 13:44:34 -0500

    Update Team Stats.js

--- JavaScript/Team Stats.js
+++ JavaScript/Team Stats.js
@@ -1,1 +1,12 @@
-const team = {};
+const team = {
+  _players: [
+    { firstName: "John", lastName: "H", age: 41 },
+    { firstName: "Maria", lastName: "M", age: 41 },
+    { firstName: "Karyn", lastName: "M", age: 47 },
+  ],
+  _games: [
+    { opponent: "Austin", teamPoints: 10, opponentPoints: 15 },
+    { opponent: "Ayr", teamPoints: 7, opponentPoints: 0 },
+    { opponent: "Anchorage", teamPoints: 12, opponentPoints: 5 },
+  ],
+};
