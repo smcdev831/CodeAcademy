48ec14074cb5083fb69e119f5c79088cb7c6b8b1
smcdev <smcdev@outlook.com>
2023-05-17 18:20:43 -0500

    Create Higher Order Functions.js

---
+++ JavaScript/Higher Order Functions.js
@@ -0,0 +1,25 @@
+const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
+  for (let i = 1; i <= 1000000; i++) {
+    if (2 + 2 != 4) {
+      console.log("Something has gone very wrong :( ");
+    }
+  }
+};
+// Write your code below
+const isTwoPlusTwo = checkThatTwoPlusTwoEqualsFourAMillionTimes;
+isTwoPlusTwo();
+console.log(isTwoPlusTwo.name);
+
+const addTwo = (num) => {
+  return num + 2;
+};
+const checkConsistentOutput = (func, val) => {
+  let checkA = val + 2;
+  let checkB = func(val);
+  if (checkA === checkB) {
+    return func(val);
+  } else {
+    return "inconsistent results";
+  }
+};
+console.log(checkConsistentOutput(addTwo, 5));
