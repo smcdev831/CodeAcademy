9e0f29151a2954018c88fb35d0973751a465fcc8
smcdev <smcdev@outlook.com>
2023-05-18 17:40:52 -0500

    Update Code Challenges Intermediate Javascript.js

--- JavaScript/Code Challenges Intermediate Javascript.js
+++ JavaScript/Code Challenges Intermediate Javascript.js
@@ -34,3 +34,21 @@ function convertToBaby(animals) {
 // When you're ready to test your code, uncomment the below and run:
 const animals = ["panda", "turtle", "giraffe", "hippo", "sloth", "human"];
 console.log(convertToBaby(animals));
+
+const numbers = [5, 3, 9, 30];
+const smallestPowerOfTwo = (arr) => {
+  let results = [];
+  // The 'outer' for loop - loops through each element in the array
+  for (let i = 0; i < arr.length; i++) {
+    number = arr[i];
+    // The 'inner' while loop - searches for smallest power of 2 greater than the given number
+    let j = 1;
+    while (j < number) {
+      j = j * 2;
+    }
+    results.push(j);
+  }
+  return results;
+};
+console.log(smallestPowerOfTwo(numbers));
+// Should print the returned array [ 8, 4, 16, 32 ] instead prints the returned array [8]
