ee7c1f9bd028879778b5e928f22695dcc1b767a0
smcdev <smcdev@outlook.com>
2023-05-17 21:46:10 -0500

    Update Iterators.js

--- JavaScript/Iterators.js
+++ JavaScript/Iterators.js
@@ -27,3 +27,63 @@ const bigNumbers = [100, 200, 300, 400, 500];
 const smallNumbers = bigNumbers.map((number) => {
   return number / 100;
 });
+
+const randomNumbers = [375, 200, 3.14, 7, 13, 852];
+// Call .filter() on randomNumbers below
+const smallNumbers = randomNumbers.filter((number) => {
+  return number < 250;
+});
+const favoriteWords = [
+  "nostalgia",
+  "hyperbole",
+  "fervent",
+  "esoteric",
+  "serene",
+];
+// Call .filter() on favoriteWords below
+const longFavoriteWords = favoriteWords.filter((word) => {
+  return word.length > 7;
+});
+
+const animals = [
+  "hippo",
+  "tiger",
+  "lion",
+  "seal",
+  "cheetah",
+  "monkey",
+  "salamander",
+  "elephant",
+];
+const foundAnimal = animals.findIndex((animal) => {
+  return animal === "elephant";
+});
+const startsWithS = animals.findIndex((animal) => {
+  return animal[0] === "s";
+});
+
+const newNumbers = [1, 3, 5, 7];
+const newSum = newNumbers.reduce((accumulator, currentValue) => {
+  console.log("The value of accumulator: ", accumulator);
+  console.log("The value of currentValue: ", currentValue);
+  return accumulator + currentValue;
+}, 10);
+console.log(newSum);
+
+const words = ["unique", "uncanny", "pique", "oxymoron", "guise"];
+// Something is missing in the method call below
+console.log(
+  words.some((word) => {
+    return word.length < 6;
+  })
+);
+// Use filter to create a new array
+const interestingWords = words.filter((word) => {
+  return word.length > 5;
+});
+// Make sure to uncomment the code below and fix the incorrect code before running it
+console.log(
+  interestingWords.every((word) => {
+    return word.length > 5;
+  })
+);
