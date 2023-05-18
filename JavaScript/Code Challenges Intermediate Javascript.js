a8ee36fda3eb58f982fd7a1bf653314ed75d50a7
smcdev <smcdev@outlook.com>
2023-05-18 18:17:10 -0500

    Update Code Challenges Intermediate Javascript.js

--- JavaScript/Code Challenges Intermediate Javascript.js
+++ JavaScript/Code Challenges Intermediate Javascript.js
@@ -68,3 +68,18 @@ function acceptEverything(veggies) {
 }
 declineEverything(veggies);
 acceptEverything(veggies);
+
+const numbers = [2, 7, 9, 171, 52, 33, 14];
+const toSquare = (num) => num * num;
+// Write your code here:
+function squareNums(numbers) {
+  return numbers.map(toSquare);
+}
+console.log(squareNums(numbers));
+
+function shoutGreetings(greetings) {
+  let loudGreetings = [];
+  for (let i = 0; i < greetings.length; i++) {
+    loudGreetings.push(greetings[i].toUppercase + "!");
+  }
+}
