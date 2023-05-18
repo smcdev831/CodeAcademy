e5609ebcdeea3109f40493ff4cb2dff2c7084759
smcdev <smcdev@outlook.com>
2023-05-17 21:21:40 -0500

    Update Iterators.js

--- JavaScript/Iterators.js
+++ JavaScript/Iterators.js
@@ -3,3 +3,27 @@ const fruits = ["mango", "papaya", "pineapple", "apple"];
 fruits.forEach((fruit) => {
   console.log(`I want to eat a ${fruit}`);
 });
+
+const animals = [
+  "Hen",
+  "elephant",
+  "llama",
+  "leopard",
+  "ostrich",
+  "Whale",
+  "octopus",
+  "rabbit",
+  "lion",
+  "dog",
+];
+
+// Create the secretMessage array below
+const secretMessage = animals.map((animal) => {
+  return animal[0];
+});
+console.log(secretMessage.join(""));
+const bigNumbers = [100, 200, 300, 400, 500];
+// Create the smallNumbers array below
+const smallNumbers = bigNumbers.map((number) => {
+  return number / 100;
+});
