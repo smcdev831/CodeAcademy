e591e744bdb671aaa9b0e260158705ce76b6cbcb
smcdev <smcdev@outlook.com>
2023-05-18 17:21:20 -0500

    Update Code Challenges Intermediate Javascript.js

---
+++ JavaScript/Code Challenges Intermediate Javascript.js
@@ -0,0 +1,12 @@
+// Write your code here:
+function reverseArray(sentence) {
+  let newSentence = [];
+  for (let i = sentence.length - 1; i >= 0; i--) {
+    newSentence.push(sentence[i]);
+  }
+  return newSentence;
+}
+// When you're ready to test your code, uncomment the below and run:
+const sentence = ["sense.", "make", "all", "will", "This"];
+console.log(reverseArray(sentence));
+// Should print ['This', 'will', 'all', 'make', 'sense.'];
