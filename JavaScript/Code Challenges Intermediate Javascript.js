15d90fe80ccd08c1512ba07ee9828047dd38d09a
smcdev <smcdev@outlook.com>
2023-05-18 19:10:32 -0500

    Update Code Challenges Intermediate Javascript.js

--- JavaScript/Code Challenges Intermediate Javascript.js
+++ JavaScript/Code Challenges Intermediate Javascript.js
@@ -89,3 +89,12 @@ function shoutGreetings(greetings) {
 const greetings = ["hello", "hi", "heya", "oi", "hey", "yo"];
 console.log(shoutGreetings(greetings));
 // Should print [ 'HELLO!', 'HI!', 'HEYA!', 'OI!', 'HEY!', 'YO!' ]
+
+// Write your code here:
+function sortYears(years) {
+  return years.sort().reverse();
+}
+// Feel free to uncomment the below code to test your function:
+const years = [1970, 1999, 1951, 1982, 1963, 2011, 2018, 1922];
+console.log(sortYears(years));
+// Should print [ 2018, 2011, 1999, 1982, 1970, 1963, 1951, 1922 ]
