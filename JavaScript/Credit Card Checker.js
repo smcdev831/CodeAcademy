3278e7f4fe3cbcd62130bddc726849b57e3b602c
smcdev <smcdev@outlook.com>
2023-05-20 19:15:11 -0500

    Update Credit Card Checker.js

--- JavaScript/Credit Card Checker.js
+++ JavaScript/Credit Card Checker.js
@@ -3,16 +3,18 @@ function validateCred(arr) {
   total = 0;
   for (let i = 0; i < newArr.length; i++) {
     if (i % 2 === 0) {
-      total += newArr[i];
-    } else {
       if (newArr[i] * 2 > 9) {
         total += newArr[i] * 2 - 9;
       } else {
         total += newArr[i] * 2;
       }
+    } else {
+      total += newArr[i];
     }
   }
-  console.log(total + arr.pop());
+  if ((total + arr.pop()) % 10 === 0) {
+    console.log("Valid");
+  } else {
+    console.log("Invalid");
+  }
 }
-
-validateCred([4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8]);
