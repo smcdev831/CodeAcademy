e102271f0daa0a93d6ec4872a30fa22b52954c7f
smcdev <smcdev@outlook.com>
2023-05-25 19:56:15 -0500

    Update script.js

---
+++ Projects/Message Generator/script.js
@@ -0,0 +1,26 @@
+function generateRandomNumber(number) {
+  return Math.floor(Math.random() * number);
+}
+
+let signs = ["Sun", "Moon", "Star"];
+let luck = [
+  "good luck",
+  "terrible luck",
+  "incredible luck",
+  "bad luck",
+  "neutral",
+];
+let status = [
+  "Go for a walk",
+  "Learn a new language",
+  "Try a new food",
+  "Listen to music",
+  "Start a journal",
+  "Visit a museum",
+  "Learn a new skill",
+  "Read a book",
+  "Watch a movie",
+  "Call a friend",
+  "Do some volunteer work",
+  "Try a new form of exercise",
+];
