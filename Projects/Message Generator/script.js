fdff81233261338610803b6666b8e4a306e2ec81
smcdev <smcdev@outlook.com>
2023-06-06 20:16:44 -0500

    Update script.js

--- Projects/Message Generator/script.js
+++ Projects/Message Generator/script.js
@@ -2,27 +2,29 @@ function generateRandomNumber(number) {
   return Math.floor(Math.random() * number);
 }
 
-let signs = ["Sun", "Moon", "Star"];
-let luck = [
-  "good luck",
-  "terrible luck",
-  "incredible luck",
-  "bad luck",
-  "neutral",
-];
-let status = [
-  "Go for a walk",
-  "Learn a new language",
-  "Try a new food",
-  "Listen to music",
-  "Start a journal",
-  "Visit a museum",
-  "Learn a new skill",
-  "Read a book",
-  "Watch a movie",
-  "Call a friend",
-  "Do some volunteer work",
-  "Try a new form of exercise",
-];
+const wisdom = {
+  signs: ["Sun", "Moon", "Star"],
+  luck: [
+    "good luck",
+    "terrible luck",
+    "incredible luck",
+    "bad luck",
+    "neutral",
+  ],
+  status: [
+    "Go for a walk",
+    "Learn a new language",
+    "Try a new food",
+    "Listen to music",
+    "Start a journal",
+    "Visit a museum",
+    "Learn a new skill",
+    "Read a book",
+    "Watch a movie",
+    "Call a friend",
+    "Do some volunteer work",
+    "Try a new form of exercise",
+  ],
+};
 
 for (let i = 0; i < arr.length; i++) {}
